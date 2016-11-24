2383ebb5bff41e03de3c43672f1078861188de76
krisrimar <rimarkris@gmail.com>
2016-11-24 21:34:31 +0200

    Added bunch of comments to HelloWorld class

--- main.java
+++ main.java
@@ -2,10 +2,19 @@
 
 /* the above comment is a docummentation and will be handled by the javadoc when there will be one */
 
+//This is the class definition
+//Each application begins with a class definition
 class HelloWorld
 {
-  public static void main(String args[])
+  //The main method
+  //In Java, every application should contain a main method
+  //The main methods signature always remains the same
+  //Just like in C++ we have the int main()
+  public static void main(String args[]) //the argument of this function can be named in any way you want, but args or argv is preferred
+  //Each string in the array above is called a command line argument
+  //They allow the user to affect the operation of an application without re-compiling it
   {
     System.out.println("Hello World!"); //prints "Hello World!"
+    //This uses the System class from the core library
   }
 }
