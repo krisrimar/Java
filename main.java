ae288b1605ccdfa6d6f5b1939ac4801e3293c815
krisrimar <rimarkris@gmail.com>
2016-11-24 21:25:24 +0200

    Initial commit

---
+++ main.java
@@ -0,0 +1,11 @@
+/** The main class for printing the string "Hello World" */
+
+/* the above comment is a docummentation and will be handled by the javadoc when there will be one */
+
+class HelloWorld
+{
+  public static void main(String args[])
+  {
+    System.out.println("Hello World!"); //prints "Hello World!"
+  }
+}
